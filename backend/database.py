175142bedb3d0aadc79cf1949970fa6451532e69
albertiadam <albertiadam2004@gmail.com>
2026-04-25 21:11:16 +0200

    Changed to online db

--- backend/database.py
+++ backend/database.py
@@ -1,9 +1,17 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
 
-DB_URL = "sqlite:///./project_db.db"
-engine = create_engine(DB_URL, connect_args={"check_same_thread": False})
+load_dotenv()
+
+DB_URL = os.getenv('DB_URL')
+connect_args = {}
+if DB_URL and "postgresql+pg8000" in DB_URL:
+    connect_args = {"ssl_context": True}
+
+engine = create_engine(DB_URL, connect_args=connect_args)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
